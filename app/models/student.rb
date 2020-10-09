06d58d3a62a0349b86b8acddbc4865b00a9ee31b
RyanBegnaud <Ryanbegnaud13@gmail.com>
2020-10-09 13:18:19 -0700

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(param)
+    if param == "" || param == ''
+      Student.all 
+    else 
+      Student.all.select {|k| k.name.downcase.include?(param)}
+    end
+      
+  end
 end
